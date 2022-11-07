554b4fd5a7e6fcc141011e84e69eef285d0d8a4c
s827 <dskumar827@gmail.com>
2022-11-07 20:36:11 +0530

    added Navbar and css applied

---
+++ src/Icons/globe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { FaGlobeAsia } from "react-icons/fa";
+
+export default function Icons() {
+    return (
+        <div>
+            <FaGlobeAsia size={28} />
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import GlobeIcon from '../Icons/globe'
+
 
 export default function Navbar() {
     return (
-        <div>Hello Nav</div>
+        <div className='navbar'>
+            <GlobeIcon className='globe-icon' />
+            <h3 className='nav-title'>My Travel Journal</h3>
+        </div>
     )
 }
