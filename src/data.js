40c1bd3ea4c0209c9901e7512fd5633b6edfb8cf
s827 <dskumar827@gmail.com>
2022-11-08 00:05:47 +0530

    added 16 travelled city's data

---
+++ src/data.js
@@ -0,0 +1,146 @@
+export default [
+    {
+        title: "Yangon",
+        location: "Myanmar",
+        googleMapsUrl: "https://goo.gl/maps/iKMfEZPEKrsHaREf6",
+        startDate: "04 Feb, 2018",
+        endDate: "15 Feb, 2018",
+        description: "Yangon (formerly known as Rangoon) is the largest city in Myanmar (formerly Burma). A mix of British colonial architecture, modern high-rises and gilded Buddhist pagodas define its skyline. Its famed Shwedagon Paya, a huge, shimmering pagoda complex, draws thousands of pilgrims annually. The city's other notable religious sites include the Botataung and Sule pagodas, both housing Buddhist relics.",
+        imageUrl: "https://www.wendywutours.com.au/resource/upload/734/banner-yangon.jpg"
+    },
+    {
+        title: "Taipei",
+        location: "Taiwan",
+        googleMapsUrl: "https://goo.gl/maps/sjiJzteFMV3TXTbNA",
+        startDate: "03 Apr, 2018",
+        endDate: "05 Apr, 2018",
+        description: "Taipei, the capital of Taiwan, is a modern metropolis with Japanese colonial lanes, busy shopping streets and contemporary buildings. The skyline is crowned by the 509m-tall, bamboo-shaped Taipei 101 skyscraper, with upscale shops at the base and a rapid elevator to an observatory near the top. Taipei is also known for its lively street-food scene and many night markets, including expansive Shilin market.",
+        imageUrl: "https://images.skyscrapercenter.com/uploads/Taipei_-2019_(CC__BY-SA)Heeheemalu210125-020151.jpg"
+    },
+    {
+        title: "Cairo",
+        location: "Egypt",
+        googleMapsUrl: "https://goo.gl/maps/AQFoNvDd65oDhqav6",
+        startDate: "11 Nov, 2018",
+        endDate: "23 Nov, 2018",
+        description: "Cairo, Egypt’s sprawling capital, is set on the Nile River. At its heart is Tahrir Square and the vast Egyptian Museum, a trove of antiquities including royal mummies and gilded King Tutankhamun artifacts. Nearby, Giza is the site of the iconic pyramids and Great Sphinx, dating to the 26th century BC. In Gezira Island’s leafy Zamalek district, 187m Cairo Tower affords panoramic city views.",
+        imageUrl: "https://www.timeoutabudhabi.com/cloud/timeoutabudhabi/2021/09/12/yjVlJCSV-Cairo_1-1.jpg"
+    },
+    {
+        title: "Ho Chi Minh City",
+        location: "Vietnam",
+        googleMapsUrl: "https://goo.gl/maps/XvMtNmzpKz5k6yvq5",
+        startDate: "10 Jan, 2019",
+        endDate: "21 Jan, 2019",
+        description: "Ho Chi Minh City, formerly known as Saigon, is the largest city in Vietnam, with a population of around 9 million in 2019. Situated in the southeast region of Vietnam, the city surrounds the Saigon River and covers about 2,061 km².",
+        imageUrl: "https://www.timeoutabudhabi.com/cloud/timeoutabudhabi/2021/09/12/yjVlJCSV-Cairo_1-1.jpg"
+    },
+    {
+        title: "Zurich",
+        location: "Switzerland",
+        googleMapsUrl: "https://goo.gl/maps/6fSLcifeCgUTHEfh7",
+        startDate: "07 Jul, 2019",
+        endDate: "24 Jul, 2019",
+        description: "The city of Zurich, a global center for banking and finance, lies at the north end of Lake Zurich in northern Switzerland. The picturesque lanes of the central Altstadt (Old Town), on either side of the Limmat River, reflect its pre-medieval history. Waterfront promenades like the Limmatquai follow the river toward the 17th-century Rathaus (town hall).",
+        imageUrl: "https://images.unsplash.com/photo-1515488764276-beab7607c1e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8enVyaWNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/YYmiomiR7HinEYMp8",
+        startDate: "06 Aug, 2019",
+        endDate: "13 Aug, 2019",
+        description: "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.",
+        imageUrl: "https://www.travelandleisure.com/thmb/CbvtaZu659SP8qOSCljNCuRby_Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg"
+    },
+    {
+        title: "Helsinki",
+        location: "Finland",
+        googleMapsUrl: "https://goo.gl/maps/si8yHP9CQxRmZJ1i6",
+        startDate: "14 Aug, 2019",
+        endDate: "21 Aug, 2019",
+        description: "Helsinki, Finland’s southern capital, sits on a peninsula in the Gulf of Finland. Its central avenue, Mannerheimintie, is flanked by institutions including the National Museum, tracing Finnish history from the Stone Age to the present. Also on Mannerheimintie are the imposing Parliament House and Kiasma, a contemporary art museum. Ornate red-brick Uspenski Cathedral overlooks a harbor.",
+        imageUrl: "https://thumbs.dreamstime.com/b/old-town-helsinki-finland-scenic-summer-evening-panorama-port-pier-architecture-tall-historical-sailing-ships-46298157.jpg"
+    },
+    {
+        title: "Dusseldorf",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/wTkJX71M6AESXnWJ8",
+        startDate: "17 Sep, 2019",
+        endDate: "20 Sep, 2019",
+        description: "Düsseldorf is a city in western Germany known for its fashion industry and art scene. It's divided by the Rhine River, with its Altstadt (Old Town) on the east bank and modern commercial areas to the west. In the Altstadt, St. Lambertus Church and Schlossturm (Castle Tower) both date to the 13th century. Streets such as Königsallee and Schadowstrasse are lined with boutique shops.",
+        imageUrl: "https://cdn.britannica.com/49/114549-050-655BC80B/river-Rhine-Tower-Germany-Dusseldorf.jpg?w=690&h=388&c=crop"
+    },
+    {
+        title: "Barcelona",
+        location: "Spain",
+        googleMapsUrl: "https://goo.gl/maps/vGL3pyCgPmR3YsK46",
+        startDate: "21 Sep, 2019",
+        endDate: "1 Oct, 2019",
+        description: "Barcelona, the cosmopolitan capital of Spain’s Catalonia region, is known for its art and architecture. The fantastical Sagrada Família church and other modernist landmarks designed by Antoni Gaudí dot the city. Museu Picasso and Fundació Joan Miró feature modern art by their namesakes. City history museum MUHBA, includes several Roman archaeological sites.",
+        imageUrl: "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F180528121341-01-barcelona-beach-guide.jpg"
+    },
+    {
+        title: "Copenhagen",
+        location: "Denmark",
+        googleMapsUrl: "https://goo.gl/maps/jCTWV2iJcyYV88E88",
+        startDate: "20 Oct, 2019",
+        endDate: "30 Oct, 2019",
+        description: "Copenhagen, Denmark’s capital, sits on the coastal islands of Zealand and Amager. It’s linked to Malmo in southern Sweden by the Öresund Bridge. Indre By, the city's historic center, contains Frederiksstaden, an 18th-century rococo district, home to the royal family’s Amalienborg Palace. Nearby is Christiansborg Palace and the Renaissance-era Rosenborg Castle, surrounded by gardens and home to the crown jewels.",
+        imageUrl: "https://d3dqioy2sca31t.cloudfront.net/Projects/cms/production/000/033/190/slideshow/515c66af69d79a2d3c9caa0bfdf29934/slide-denmark-copenhagen-nyhavn.jpg"
+    },
+    {
+        title: "Oslo",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/EpNe8J6jH9ccwVJd6",
+        startDate: "30 Oct, 2019",
+        endDate: "1=20 Nov, 2019",
+        description: "Oslo, the capital of Norway, sits on the country’s southern coast at the head of the Oslofjord. It’s known for its green spaces and museums. Many of these are on the Bygdøy Peninsula, including the waterside Norwegian Maritime Museum and the Viking Ship Museum, with Viking ships from the 9th century. The Holmenkollbakken is a ski-jumping hill with panoramic views of the fjord. It also has a ski museum.",
+        imageUrl: "https://assets.simpleviewcms.com/simpleview/image/fetch/c_fill,f_jpg,h_605,q_65,w_1200/https://media.newmindmedia.com/TellUs/image/%3Ffile%3DNic_Lehoux_1702714961.jpg%26dh%3D533%26dw%3D800%26cropX%3D614%26cropY%3D0%26cropH%3D3334%26cropW%3D5001%26t%3D4"
+    },
+    {
+        title: "Prague",
+        location: "Czechia",
+        googleMapsUrl: "https://goo.gl/maps/UW5iUvoeS5LtwNmC9",
+        startDate: "20 Nov, 2019",
+        endDate: "23 Nov, 2019",
+        description: "Prague, capital city of the Czech Republic, is bisected by the Vltava River. Nicknamed “the City of a Hundred Spires,” it's known for its Old Town Square, the heart of its historic core, with colorful baroque buildings, Gothic churches and the medieval Astronomical Clock, which gives an animated hourly show. Completed in 1402, pedestrian Charles Bridge is lined with statues of Catholic saints.",
+        imageUrl: "https://thumbs.dreamstime.com/b/bridges-prague-czech-republic-scenic-summer-aerial-view-old-town-pier-architecture-charles-bridge-over-vltava-river-42628509.jpg"
+    },
+    {
+        title: "Stockholm",
+        location: "Sweden",
+        googleMapsUrl: "https://goo.gl/maps/F6JcCxUzaCySdta8A",
+        startDate: "23 Nov, 2019",
+        endDate: "04 Dec, 2019",
+        description: "Stockholm, the capital of Sweden, encompasses 14 islands and more than 50 bridges on an extensive Baltic Sea archipelago. The cobblestone streets and ochre-colored buildings of Gamla Stan (the old town) are home to the 13th-century Storkyrkan Cathedral, the Kungliga Slottet Royal Palace and the Nobel Museum, which focuses on the Nobel Prize. Ferries and sightseeing boats shuttle passengers between the islands.",
+        imageUrl: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iUE0rp9aSBa0/v1/800x-1.jpg"
+    },
+    {
+        title: "Manila",
+        location: "Philippines",
+        googleMapsUrl: "https://goo.gl/maps/Zim18bGk43EzC4Wc8",
+        startDate: "01 Jan, 2020",
+        endDate: "10 Jan, 2020",
+        description: "Metropolitan Manila, officially the National Capital Region, is the seat of government and one of three defined metropolitan areas in the Philippines.",
+        imageUrl: "https://content.r9cdn.net/rimg/dimg/3f/25/1f5a7b8e-city-29645-176cf1dd5ad.jpg?crop=true&width=1020&height=498"
+    },
+    {
+        title: "Minsk",
+        location: "Belarus",
+        googleMapsUrl: "https://goo.gl/maps/8WxgFNjFTtLHjPQk7",
+        startDate: "16 Jan, 2020",
+        endDate: "10 Feb, 2020",
+        description: "Minsk is the capital and the largest city of Belarus, located on the Svislach and the now subterranean Niamiha rivers. As the capital, Minsk has a special administrative status in Belarus and is the administrative centre of Minsk Region and Minsk District.",
+        imageUrl: "https://www.intrepidtravel.com/adventures/wp-content/uploads/2019/06/shutterstock_1282471396.jpg"
+    },
+    {
+        title: "Bucharest",
+        location: "Romania",
+        googleMapsUrl: "https://goo.gl/maps/KqTuBCDZPKAMeo3E8",
+        startDate: "01 Mar, 2020",
+        endDate: "16 Mar, 2020",
+        description: "Bucharest, in southern Romania, is the country's capital and commercial center. Its iconic landmark is the massive, communist-era Palatul Parlamentului government building, which has 1,100 rooms. Nearby, the historic Lipscani district is home to an energetic nightlife scene as well as tiny Eastern Orthodox Stavropoleos Church and 15th-century Curtea Veche Palace, where Prince Vlad III (“The Impaler”) once ruled.",
+        imageUrl: "https://www.ezarri.com/media/uploads/noticias/EXD_Fantani-531.jpg"
+    }
+]
