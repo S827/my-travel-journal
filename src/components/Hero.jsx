790c82566bf2031028bc6b5e5b8988cb0f4e46e9
s827 <dskumar827@gmail.com>
2022-11-08 11:55:58 +0530

    Added All the Hero contents in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,17 @@ import Map from '../Icons/map'
 export default function Hero(props) {
     return (
         <div className='hero-div'>
-            {props.item.title}
+            
             <div>
                 <img className='location-img' src={props.item.imageUrl} />
             </div>
             <div>
                 <Map />
+                <span>{props.item.location}</span>
+                <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+                <h1>{props.item.title}</h1>
+                <h3>{props.item.startDate}-{props.item.endDate}</h3>
+                <p>{props.item.description}</p>
             </div>
         </div>
     )
